9457e8aa0abefd466570fbbd3601e911c2d48d43
Alejandro Taboada <ataboada@gmail.com>
2017-07-13 19:58:03 +0000

    modifications to execute scripts

---
+++ commands/configure_gateway_custom_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ROOT_PASSWORD=$1
+
+set_root_password.sh $ROOT_PASSWORD
+
--- configure_safewalk_custom_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ROOT_PASS=$1
-
-usermod --password $(echo $ROOT_PASS | openssl passwd -1 -stdin) root
-
