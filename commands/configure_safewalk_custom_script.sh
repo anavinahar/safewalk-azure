3a40552a8ef5cebfdf02e8d8489cb1dca352b0c7
root <root@safewalk-bdr.(none)>
2017-07-21 18:35:41 -0300

    Partitions

--- commands/configure_safewalk_custom_script.sh
+++ commands/configure_safewalk_custom_script.sh
@@ -8,6 +8,8 @@ SAFEWALK_IP=$5
 
 #sh configure_safewalk_custom_script.sh root 192.168.10.244 192.168.10.244 Safewalk1 192.168.10.201
 
+sh safewalk_make_partitions.sh
+
 my_dir=`dirname $0`
 sh $my_dir/set_root_password.sh $ROOT_PASSWORD
 bash $my_dir/safewalk_create_gateway.sh --gateway-name "My Gateway" --gateway-password $GATEWAY_ROOT_PASSWORD --gateway-public-host $GATEWAY_PUBLIC_IP --gateway-ssh-host $GATEWAY_IP --safewalk-host $SAFEWALK_IP
