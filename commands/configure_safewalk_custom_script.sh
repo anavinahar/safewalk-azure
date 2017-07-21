752877abc29274e624a1d978c255d3ecd0920883
Alejandro Taboada <ataboada@gmail.com>
2017-07-21 16:17:04 -0300

    add command to make the gateway configuration

--- commands/configure_safewalk_custom_script.sh
+++ commands/configure_safewalk_custom_script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-ROOT_PASS=$1
-my_dir=`dirname $0`                
-$my_dir/usermod --password $(echo $ROOT_PASS | openssl passwd -1 -stdin) root
+ROOT_PASSWORD=$1
+GATEWAY_IP=$2
+GATEWAY_PUBLIC_IP=$3
+GATEWAY_ROOT_PASSWORD=$4
+SAFEWALK_IP=$5
 
+my_dir=`dirname $0`
+$my_dir/set_root_password.sh $ROOT_PASSWORD
+$my_dir/create_safewalk_gateway.sh --gateway-name "My Gateway" --gateway-password $GATEWAY_ROOT_PASSWORD --gateway-public-host $GATEWAY_PUBLIC_IP --gateway-ssh-host $GATEWAY_IP --safewalk-host $SAFEWALK_IP
