f2c29694ce028bb565a4a5d715fa8e667a78f1f2
Alejandro Taboada <ataboada@gmail.com>
2017-07-25 13:21:01 +0000

    fix radius service type

--- commands/safewalk_renew_secrets.sh
+++ commands/safewalk_renew_secrets.sh
@@ -4,6 +4,6 @@ bash /home/safewalk/safewalk_server/sources/bin/update_fs_secrets_from_db
 service apache2 restart
 service postgresql restart
 service supervisor restart
-service gaiaradius stop
+service gaiaradiusd stop
 sleep 15
-service gaiaradius start
+service gaiaradiusd start
