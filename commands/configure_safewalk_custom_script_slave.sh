60fa8a0c987c5ecb094133f1fcd8600537827fad
Alejandro Taboada <ataboada@gmail.com>
2017-07-26 20:32:28 +0000

    fixes

--- commands/configure_safewalk_custom_script_slave.sh
+++ commands/configure_safewalk_custom_script_slave.sh
@@ -19,4 +19,7 @@ sh $my_dir/safewalk_renew_secrets.sh
 
 #bash $my_dir/safewalk_create_gateway.sh --gateway-name "My Gateway" --gateway-password $GATEWAY_ROOT_PASSWORD --gateway-public-host $GATEWAY_PUBLIC_IP --gateway-ssh-host $GATEWAY_IP --safewalk-host $SAFEWALK_IP_1
 
+cp $my_dir/bdr_join_node /home/safewalk/safewalk_server/sources/bin/bdr_join_node
+chmod +x /home/safewalk/safewalk_server/sources/bin/bdr_join_node
+chown safewalk:safewalk /home/safewalk/safewalk_server/sources/bin/bdr_join_node
 bash safewalk_bdr_join.sh $SAFEWALK_IP_2 $SAFEWALK_IP_1
